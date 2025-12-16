9972dfccfa0ae01200b2461c50cb215db73337af
unknown <gigihhaidarfalah14@gmail.com>
2025-12-16 21:04:54 +0700

    Final Script py

--- Detection and Track/main.py
+++ Detection and Track/main.py
@@ -7,16 +7,16 @@ model = YOLO("best150.pt")
 names = model.names
 
 # X position of the vertical counting line
-line_y = 378
+line_y = 550
 
 # Store previous center positions for tracking
 track_hist = {}
 
 # Vehicle counters
 car_in = 0
-car_out = 0
+# car_out = 0
 mtc_in = 0
-mtc_out = 0
+# mtc_out = 0
 
 # Open video file or webcam (use 0 for webcam)
 cap = cv2.VideoCapture("1div4-videojalanraya4.mp4")
@@ -41,7 +41,7 @@ while True:
     frame = cv2.resize(frame, (800, 600))
 
     # Detect and track class
-    results = model.track(frame, persist=True, classes=[1,2])
+    results = model.track(frame, persist=True, classes=[0,1])
 
     if results[0].boxes.id is not None:
         ids = results[0].boxes.id.cpu().numpy().astype(int)
@@ -50,21 +50,42 @@ while True:
         for box,track_id,class_id in zip(boxes,ids,class_ids):
             x1, y1, x2, y2 = box
             name=names[class_id]
+            
+            cx=int((x1+x2)/2)
+            cy=int((y1+y2)/2)
+            if track_id in track_hist:
+                prev_cx, prev_cy = track_hist[track_id]
+                if(prev_cy < line_y<=cy):
+                    if 'car'in name:
+                        cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),2)
+                        car_in += 1
+                    if 'Motorcycle' in name:
+                        cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),2)
+                        mtc_in += 1
+                # elif(prev_cy > line_y>=cy):
+                #     if 'car' in name:
+                #         car_out += 1
+                #     elif 'motorcycle' in name:
+                #         mtc_out += 1
+            
             cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
-
+            cv2.circle(frame,(cx,cy),4,(255,0,0),-1)
+            cvzone.putTextRect(frame,f'{name}',(x1,y1),scale=2,thickness=2,colorT=(255,255,255),colorR=(0,128,0))
+            cvzone.putTextRect(frame,f'{track_id}',(x2,y2),scale=2,thickness=2,colorT=(255,255,255),colorR=(0,128,120))
+            track_hist[track_id] = (cx, cy)
 
 
     
     cvzone.putTextRect(frame, f'car_in: {car_in}', (60, 40), scale=2, thickness=2,
                    colorT=(255, 255, 255), colorR=(0, 128, 0))
-    cvzone.putTextRect(frame, f'car_out: {car_out}', (640, 40), scale=2, thickness=2,
-                   colorT=(255, 255, 255), colorR=(0, 0, 120))
+    # cvzone.putTextRect(frame, f'car_out: {car_out}', (640, 40), scale=2, thickness=2,
+    #                colorT=(255, 255, 255), colorR=(0, 0, 120))
     cvzone.putTextRect(frame, f'mtc_in: {mtc_in}', (60, 90), scale=2, thickness=2,
                     colorT=(255, 255, 255), colorR=(120, 0, 120))
-    cvzone.putTextRect(frame, f'mtc_out: {mtc_out}', (640, 90), scale=2, thickness=2,
-                    colorT=(255, 255, 255), colorR=(0, 120, 120))
+    # cvzone.putTextRect(frame, f'mtc_out: {mtc_out}', (640, 90), scale=2, thickness=2,
+    #                 colorT=(255, 255, 255), colorR=(0, 120, 120))
 
-
+    print(track_hist)
     cv2.line(frame, (0, line_y), (frame.shape[1], line_y), (255, 255, 255), 2)
     print(frame.shape)
 
